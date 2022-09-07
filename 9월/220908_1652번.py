a3e2bea477a551a64f027eb1121d8d72ce6a6eca
Minji Kim <moo_miz@naver.com>
2022-09-08 01:32:17 +0900

    0907

---
+++ 9월/220908_1652번.py
@@ -0,0 +1,17 @@
+
+N = int(input())
+room = [list(map(str,input().rstrip())) for _ in range(N)]
+row_cnt = 0
+col_cnt = 0
+for i in range(N):
+    for j in range(N-1):
+        if room[i][j] == '.' and room[i][j+1] == '.':
+            row_cnt += 1
+            break
+for i in range(N):
+    for j in range(N-1):
+        if room[j][i] == '.' and room[j+1][i] == '.':
+            col_cnt += 1
+            break
+print(row_cnt,col_cnt)
+# 반례를 모르겠어요 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
