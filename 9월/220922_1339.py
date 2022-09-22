a461a6778bc629a1c1fbb9baef563f220508479b
Minji Kim <moo_miz@naver.com>
2022-09-23 04:39:57 +0900

    0922

--- 9월/220922_1339.py
+++ 9월/220922_1339.py
@@ -1,4 +1,24 @@
 # 단어 수학
 N = int(input())
-arr =([input()] for _ in range(N))
-print(arr)
+alphabets = []
+alpha_dict = {}
+
+for _ in range(N):
+    alphabets.append(input().rstrip())
+
+for alphabet in alphabets:
+    square_root = len(alphabet) - 1
+    for char in alphabet:
+        if char in alpha_dict:
+            alpha_dict[char] += pow(10, square_root)
+        else:
+            alpha_dict[char] = pow(10, square_root)        
+        square_root -= 1
+
+alpha_dict = sorted(alpha_dict.values(), reverse=True)
+result, _max = 0, 9
+for i in alpha_dict:
+    result += i * _max
+    _max -= 1
+print (result)
+# 계속 시도했는데 안되스 구글링 했습니다.. 내일 다시 풀어보겠습니다 ㅠ
