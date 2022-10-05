6f3df92295e01122d51e8d13bd5acdbbc318aca4
Minji Kim <moo_miz@naver.com>
2022-10-06 00:28:03 +0900

    1006

---
+++ 10월/221006_1100.py
@@ -0,0 +1,10 @@
+arr =[]
+for i in range(8):
+    arr.append(list(map(str,input())))
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if (i+j) % 2 == 0:
+            if arr[i][j] == 'F':
+                cnt += 1
+print(cnt)
